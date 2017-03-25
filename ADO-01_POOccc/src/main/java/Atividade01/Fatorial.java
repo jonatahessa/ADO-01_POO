c64268ed655076cfe4a1d6d3392ff41b0fbce63e
jonata <jonata@ArchLinux.home>
2017-03-25 13:30:12 +0000

    Activity 1 with 2 numerics progressions complete.

--- ADO-01_POOccc/src/main/java/Atividade01/Fatorial.java
+++ ADO-01_POOccc/src/main/java/Atividade01/Fatorial.java
@@ -10,5 +10,32 @@ package Atividade01;
  * @author jonata
  */
 public class Fatorial {
-    
+
+    private int inicial;
+    private int increment;
+
+    public Fatorial() {
+        this.inicial = 1;
+    }
+
+    public void proximoValor() {
+        increment++;
+        inicial = inicial * increment;
+    }
+
+    public int valorAtual() {
+        return inicial;
+    }
+
+    public static void main(String[] args) {
+        int quantidade = 20;
+
+        Fatorial ft = new Fatorial();
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print(ft.valorAtual() + " ");
+            ft.proximoValor();
+        }
+    }
+
 }
